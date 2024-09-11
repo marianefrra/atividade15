ca989de67ad2cdf821b1944c08502f469626cfa0
marianefrra] <marianerochaferreira48@gmail.com>
2024-09-11 20:24:36 -0300

    atividade15

---
+++ atividade15.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois números e uma operação (soma, subtração, multiplicação ou divisão) e execute a operação desejada.
+
+n1 = float(input("digite o primiro número"))
+n2 = float(input("digite o segundo número"))
+
+multi = (n1*n2)
+print (f"o resultado final é = {multi}")
